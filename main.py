52ffa86d05a413e61773abffd87fe696624708cd
rutokenneth <rutokenneth16@gmail.com>
2024-11-21 17:50:52 +0300

    Character counter works 😍

--- main.py
+++ main.py
@@ -12,12 +12,24 @@ def word_count():
     words = text.split()
     return len(words)
 
+def char_count():
+    letter_count = {}
+    book_text = get_content()
+    for char in book_text:
+        char = char.lower()
+        if char.isalpha():
+            letter_count[char] = letter_count.get(char, 0) + 1
+    for letter, count in letter_count.items():
+        print(f"The letter '{letter}' appears {count} times.")
+    
+
 def main():
     text = get_content()
     print(text)
     # word count
     num_words = word_count()
     print(num_words)
+    char_count()
 
 if __name__ == '__main__':
     main()
