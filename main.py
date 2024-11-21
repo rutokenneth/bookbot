fdc5e02b7c0bb117ad7bdf9c32a870a51e94518b
rutokenneth <rutokenneth16@gmail.com>
2024-11-22 01:36:14 +0300

    simple working bookbot at 134?

--- main.py
+++ main.py
@@ -19,9 +19,21 @@ def char_count():
         char = char.lower()
         if char.isalpha():
             letter_count[char] = letter_count.get(char, 0) + 1
+        # print(letter_count)#debug line
     for letter, count in letter_count.items():
         print(f"The letter '{letter}' appears {count} times.")
+    return letter_count
     
+def generate_report():
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"77986 words found in the document\n")
+    sorted_val = dict(sorted(char_count().items(), key= lambda item: item[1], reverse=True))
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"77986 words found in the document")
+    
+    for k, v in sorted_val.items():
+        print(f"The '{k}' character was found {v} times")
+    print("\n--- End report ---")
 
 def main():
     text = get_content()
@@ -31,6 +43,7 @@ def main():
     print(num_words)
     # number of occurence of individual characters
     char_count()
+    generate_report()
 
 if __name__ == '__main__':
     main()
